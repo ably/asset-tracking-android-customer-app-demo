f356e0f66a8ada50027e447e8c01998a1fd680bd
Igor Kurek <ikurek@users.noreply.github.com>
2022-05-30 14:22:00 +0200

    Updated dependencies and removed obsolete test files

--- app/src/main/java/com/ably/tracking/demo/subscriber/MainActivity.kt
+++ app/src/main/java/com/ably/tracking/demo/subscriber/MainActivity.kt
@@ -3,41 +3,11 @@ package com.ably.tracking.demo.subscriber
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.ably.tracking.demo.subscriber.ui.theme.AATSubscriberDemoTheme
+import com.ably.tracking.demo.subscriber.ui.screen.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            AATSubscriberDemoTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    AATSubscriberDemoTheme {
-        Greeting("Android")
+        setContent { MainScreen() }
     }
 }
---
+++ app/src/main/java/com/ably/tracking/demo/subscriber/ui/screen/MainScreen.kt
@@ -0,0 +1,36 @@
+package com.ably.tracking.demo.subscriber.ui.screen
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.ably.tracking.demo.subscriber.ui.theme.AATSubscriberDemoTheme
+
+@Composable
+fun MainScreen() {
+    AATSubscriberDemoTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colors.background
+        ) {
+            Greeting("Android")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    AATSubscriberDemoTheme {
+        Greeting("Android")
+    }
+}
