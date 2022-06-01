50e49f0ffa68b1d8897336a53d8b16b25a9269bf
Igor Kurek <ikurek@users.noreply.github.com>
2022-06-01 15:45:57 +0200

    Fixed Detekt newline issues

--- app/src/main/java/com/ably/tracking/demo/subscriber/MainActivity.kt
+++ app/src/main/java/com/ably/tracking/demo/subscriber/MainActivity.kt
@@ -8,7 +8,6 @@ import com.ably.tracking.demo.subscriber.ui.screen.MainScreen
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        BuildConfig.ABLY_API_KEY
         setContent { MainScreen() }
     }
 }
