30f656e3873ad2f2dd8b2cb2a568c29956e9ecff
Igor Kurek <ikurek@users.noreply.github.com>
2022-06-06 16:28:43 +0200

    Added compose routing

--- app/src/main/java/com/ably/tracking/demo/subscriber/MainActivity.kt
+++ app/src/main/java/com/ably/tracking/demo/subscriber/MainActivity.kt
@@ -3,11 +3,20 @@ package com.ably.tracking.demo.subscriber
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.ably.tracking.demo.subscriber.ui.screen.main.MainScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { MainScreen() }
+        setContent {
+            val navController: NavHostController = rememberNavController()
+            NavHost(navController = navController, startDestination = "main") {
+                composable("main") { MainScreen() }
+            }
+        }
     }
 }
