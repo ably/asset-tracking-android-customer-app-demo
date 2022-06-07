a907bec031cba707d940b624c38f3b55d0e35da3
Igor Kurek <ikurek@users.noreply.github.com>
2022-06-07 18:02:25 +0200

    Implemented base for Hilt DI

---
+++ app/src/main/java/com/ably/tracking/demo/subscriber/AatSubscriberDemoApplication.kt
@@ -0,0 +1,7 @@
+package com.ably.tracking.demo.subscriber
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class AatSubscriberDemoApplication : Application()
